443e28185475c81225cde31ba7e517b794f0b258
114586 <114586@leerling.calandlyceum.nl>
2017-11-10 16:09:57 +0100

    Only one route
    as example of communication between front-end and back-end

--- flask/main.py
+++ flask/main.py
@@ -7,21 +7,11 @@ def index():
     # stuurt inteface naar browser.
     return app.send_static_file('index.html')
 
-@app.route('/light', methods=['GET'])
-def light():
-    data = {}
-    #zet licht aan. Voeg commando's toe!
-    #bevestig licht aan.
-    data['light'] = 'ON'
-    # converteer dict naat json en print naar interface.
-    return jsonify(data) 
-
 @app.route('/light', methods=['POST'])
 def lightPOST():
     data = request.get_json()
     data['light'] = not data['light']
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
